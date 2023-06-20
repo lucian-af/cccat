bc531fbd170eb45e9ea957cf615b15a212791867
Lucian AF <lucian_af@hotmail.com>
2023-06-20 20:00:24 -0300

    refator(autenticação): movido arquivo DTO para melhor organização;

---
+++ backend/dotnet/src/Cccat.Autenticacao/Cccat.Autenticacao.Application/Models/CadastraUsuarioOutputDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Cccat.Autenticacao.Application.UseCases
+{
+	public class CadastraUsuarioOutputDto
+	{
+		public Guid IdUsuario { get; set; }
+	}
+}
--- backend/dotnet/src/Cccat.Autenticacao/Cccat.Autenticacao.Application/UseCases/CadastraUsuario.cs
+++ backend/dotnet/src/Cccat.Autenticacao/Cccat.Autenticacao.Application/UseCases/CadastraUsuario.cs
@@ -12,17 +12,12 @@ namespace Cccat.Autenticacao.Application.UseCases
 		public CadastraUsuario(IRepositoryFactory factory)
 			=> _usuarioRepository = factory.CriarUsuarioRepository();
 
-		public async Task<Output> Cadastrar(CadastraUsuarioInputDto input)
+		public async Task<CadastraUsuarioOutputDto> Cadastrar(CadastraUsuarioInputDto input)
 		{
 			var usuario = Usuario.Criar(input.Email, input.Senha, SenhaTipo.Pbkdf2);
 			await _usuarioRepository.Cadastrar(usuario);
 
-			return new Output { IdUsuario = usuario.Id };
+			return new CadastraUsuarioOutputDto { IdUsuario = usuario.Id };
 		}
-	}
-
-	public class Output
-	{
-		public Guid IdUsuario { get; set; }
 	}
 }
